4559ff09ff9a8e6b2972c838d6bf2b65093e8c55
Isaiah Magett <imagett10@gmail.com>
2018-12-29 02:29:55 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,7 +1,8 @@
 require_relative '../current_age_for_birth_year.rb'
 
 describe 
-"current_age_for_birth_year" do it "returns the age of aperson based on the year of birth" do age_of_person = current_age_for_birth_year(1984)
+"current_age_for_birth_year" do 
+  it "returns the age of aperson based on the year of birth" do age_of_person = current_age_for_birth_year(1984)
 expedct (age_of_person) .to eq(19)
 end 
 end
