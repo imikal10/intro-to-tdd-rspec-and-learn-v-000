30deb54a5f320835345541623a156e0ee1f8ba83
Isaiah Magett <imagett10@gmail.com>
2018-12-29 02:28:06 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,1 +1,3 @@
 require_relative '../current_age_for_birth_year.rb'
+
+describe "current_age_for_birth_year" do it "returns the age of aperson based on the year of birth" do age_of_person = current_age_for_birth_year(1984)
